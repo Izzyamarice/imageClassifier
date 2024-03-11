5cb7687b4ed28a8b4bc301a4753c0972f0faff27
Isabella <80723693+Izzyamarice@users.noreply.github.com>
2024-03-11 17:39:30 +0000

    Base files uploaded

---
+++ main.py
@@ -0,0 +1,14 @@
+#type imageClassifier\Scripts\activate to run the virtual environment
+#and also python -m venc imageClassifier if it needs creating again
+
+
+#prepare
+
+
+#train/test split
+
+
+#train classifier
+
+
+#test performance
